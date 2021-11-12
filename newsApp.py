06373361bb2b8db47560987992e258a3fb039a5e
svanna <anais.dehand@gmail.com>
2021-11-12 12:21:24 +0100

    title news aggregator done

--- newsApp.py
+++ newsApp.py
@@ -4,8 +4,20 @@ import tkinter as tk
 
 def getNews():
     api_key = "f9ce68a95d4544c29eb19c07b2174f09"
-    url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=" + api_key
+    url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=" + api_key
     news = requests.get(url).json()
+
+    articles = news["articles"]
+    my_articles = []
+    my_news= ""
+
+    for article in articles:
+        my_articles.append(article["title"])
+
+    for i in range(10):
+        my_news = my_news + str(i+1) + "." + my_articles[i] + "\n"
+
+    label.config(text = my_news)
 
 
 canvas = tk.Tk()
@@ -17,5 +29,7 @@ button.pack(pady=20)
 
 label = tk.Label(canvas, font=18, justify="left")
 label.pack(pady=20)
+
+getNews()
 
 canvas.mainloop()

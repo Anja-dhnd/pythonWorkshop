dbe7b99c6ff78c4f97ebb4f75fc411bb0ed61c04
svanna <anais.dehand@gmail.com>
2021-11-14 14:40:55 +0100

    venv init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+newsApp.py,d/6/d67b223d5716c6ad7161b62b3a8e1d89c2de9096
---
+++ redditApp.py
@@ -0,0 +1,14 @@
+from pprint import pprint
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import praw
+
+user_agent = "Scraper 1.0"
+reddit = praaw.Reddit(
+    client_id="9alNpa66PPgwK9WRepyZxA",
+    client_secret="DgfKOtW36RX6kDEvGyXx6wxCCZW7sA",
+    user_agent=user_agent
+)
